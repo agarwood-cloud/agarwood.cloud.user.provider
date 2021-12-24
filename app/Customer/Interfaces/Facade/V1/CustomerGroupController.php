3dc8bc0c4354ab4248b5eeeffbbbb260d3f7b6fe
agarwood <676786620@qq.com>
2021-12-24 15:19:33 +0800

    fixed:fixed customer group bug」

--- app/Customer/Interfaces/Facade/V1/CustomerGroupController.php
+++ app/Customer/Interfaces/Facade/V1/CustomerGroupController.php
@@ -17,6 +17,7 @@ use App\Customer\Interfaces\DTO\Group\CustomerGroupCreateDTO;
 use App\Customer\Interfaces\DTO\Group\CustomerGroupIndexDTO;
 use App\Customer\Interfaces\DTO\Group\CustomerGroupUpdateDTO;
 use App\Support\Middleware\OAuthJWTMiddleware;
+use App\Support\OfficialAccountQueryParams;
 use App\Support\ParsingToken;
 use Swoft\Http\Message\Request;
 use Swoft\Http\Message\Response;
@@ -47,9 +48,17 @@ class CustomerGroupController extends AbstractBaseController
      * 应用层
      *
      * @\Swoft\Bean\Annotation\Mapping\Inject()
-     * @var CustomerGroupApplication
+     *
+     * @var \App\Customer\Application\CustomerGroupApplication
      */
     protected CustomerGroupApplication $application;
+
+    /**
+     * @\Swoft\Bean\Annotation\Mapping\Inject()
+     *
+     * @var \App\Support\OfficialAccountQueryParams
+     */
+    public OfficialAccountQueryParams $officialAccountQueryParams;
 
     /**
      * 客服分组列表
@@ -64,7 +73,10 @@ class CustomerGroupController extends AbstractBaseController
     {
         $dto = CustomerGroupAssembler::attributesToIndexDTO($request->getQueryParams());
         return $this->wrapper()->setData(
-            $this->application->indexProvider($this->getServiceUuid(''), $dto)
+            $this->application->indexProvider(
+                (int) $this->officialAccountQueryParams->getOfficialAccountId(),
+                $dto
+            )
         )->response();
     }
 
@@ -83,7 +95,10 @@ class CustomerGroupController extends AbstractBaseController
     {
         $DTO = CustomerGroupAssembler::attributesToCreateDTO((array)$request->getParsedBody());
         return $this->wrapper()->setData(
-            $this->application->createProvider($this->getServiceUuid(''), $DTO)
+            $this->application->createProvider(
+                (int) $this->officialAccountQueryParams->getOfficialAccountId(),
+                $DTO
+            )
         )->response($response->withStatus(201));
     }
 
@@ -91,48 +106,48 @@ class CustomerGroupController extends AbstractBaseController
      * 更新客服分组信息
      *
      * @param Request $request
-     * @param string  $uuid
+     * @param int  $id
      *
      * @return Response|null
      * @Validate(validator=CustomerGroupUpdateDTO::class, type=ValidateType::BODY)
-     * @RequestMapping(route="customer-group/{uuid}", method={RequestMethod::PUT, RequestMethod::PATCH})
+     * @RequestMapping(route="customer-group/{id}", method={RequestMethod::PUT, RequestMethod::PATCH})
      */
-    public function actionUpdate(Request $request, string $uuid): ?Response
+    public function actionUpdate(Request $request, int $id): ?Response
     {
         $DTO = CustomerGroupAssembler::attributesToUpdateDTO((array)$request->getParsedBody());
         return $this->wrapper()->setData(
-            $this->application->updateProvider($uuid, $DTO)
+            $this->application->updateProvider($id, $DTO)
         )->response();
     }
 
     /**
      * 删除客服分组信息
      *
-     * @RequestMapping(route="customer-group/{uuids}", method={ RequestMethod::DELETE},params={"uuids"="[A-Za-z0-9_,-]+"})
-     * @param string   $uuids
+     * @RequestMapping(route="customer-group/{ids}", method={ RequestMethod::DELETE})
+     * @param string   $ids
      * @param Response $response
      *
      * @return Response|null
      */
-    public function actionDelete(string $uuids, Response $response): ?Response
+    public function actionDelete(string $ids, Response $response): ?Response
     {
         return $this->wrapper()->setData([
-            'result' => $this->application->deleteProvider($uuids)
+            'result' => $this->application->deleteProvider($ids)
         ])->response($response->withStatus(204));
     }
 
     /**
      * 查看客服分组详情
      *
-     * @RequestMapping(route="customer-group/{uuid}", method={RequestMethod::GET},params={"uuid"="[A-Za-z0-9_-]+"})
-     * @param string $uuid
+     * @RequestMapping(route="customer-group/{id}", method={RequestMethod::GET})
+     * @param int $id
      *
      * @return Response|null
      */
-    public function actionView(string $uuid): ?Response
+    public function actionView(int $id): ?Response
     {
         return $this->wrapper()->setData(
-            $this->application->viewProvider($uuid)
+            $this->application->viewProvider($id)
         )->response();
     }
 }
